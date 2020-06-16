f594c35e6857809307287a812a311a666e8cbe75
idmacleod <idmacleod@gmail.com>
2020-06-16 16:24:33 +0100

    Setting up BookTest

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,10 @@
+import org.junit.Before;
+
 public class BookTest {
+    private Book book;
+
+    @Before
+    public void before() {
+        book = new Book("The Missing of Clairdelune", "Christelle Dabos", "Fantasy");
+    }
 }
