7912ccbb1ca46fc8e86daf44c6a02d22e3c7a01d
idmacleod <idmacleod@gmail.com>
2020-06-16 17:19:40 +0100

    Added constructor and basic methods to Borrower class

--- src/main/java/Borrower.java
+++ src/main/java/Borrower.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+
 public class Borrower {
+    private String name;
+    private ArrayList<Book> collection;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.collection = new ArrayList<Book>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int booksBorrowed() {
+        return this.collection.size();
+    }
 }
