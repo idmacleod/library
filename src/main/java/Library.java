8dc57916018b1cbd1b587e6cd40d1d5a282ef1b2
idmacleod <idmacleod@gmail.com>
2020-06-16 16:42:51 +0100

    Adding constructor to Library class

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+
 public class Library {
+    private ArrayList<Book> collection;
+    private int capacity;
+
+    public Library(int capacity) {
+        this.collection = new ArrayList<Book>();
+        this.capacity = capacity;
+    }
 }
