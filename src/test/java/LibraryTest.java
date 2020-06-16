a5ff69e79a79c15292cfdf7adc53b78d3dc8c2e3
idmacleod <idmacleod@gmail.com>
2020-06-16 17:04:58 +0100

    Added test to ensure books cannot be added if stock full

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -7,12 +7,14 @@ public class LibraryTest {
     private Library library;
     private Book book1;
     private Book book2;
+    private Book book3;
 
     @Before
     public void before() {
         library = new Library(2);
         book1 = new Book("The Missing of Clairdelune", "Christelle Dabos", "Fantasy");
         book2 = new Book("A Memory Called Empire", "Arkady Martine", "Science Fiction");
+        book3 = new Book("Moominland Midwinter", "Tove Jansson", "Children's");
     }
 
     @Test
@@ -37,5 +39,13 @@ public class LibraryTest {
     public void canCheckStockNotFull() {
         library.addBook(book1);
         assertFalse(library.stockFull());
+    }
+
+    @Test
+    public void cannotAddBookIfStockFull() {
+        library.addBook(book1);
+        library.addBook(book2);
+        library.addBook(book3);
+        assertEquals(2, library.countBooks());
     }
 }
