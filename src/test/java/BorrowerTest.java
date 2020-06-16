5d106ab7555c429f284d5f1a83cec87b05bd31b9
idmacleod <idmacleod@gmail.com>
2020-06-16 17:16:03 +0100

    Added basic tests to BorrowerTest

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -1,2 +1,23 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BorrowerTest {
+    private Borrower borrower;
+
+    @Before
+    public void before() {
+        borrower = new Borrower("Iain");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Iain", borrower.getName());
+    }
+
+    @Test
+    public void collectionStartsEmpty() {
+        assertEquals(0, borrower.booksBorrowed());
+    }
 }
