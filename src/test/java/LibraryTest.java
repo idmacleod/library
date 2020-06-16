80c6e1dca7a47b35202859bc43fe156dc7976c40
idmacleod <idmacleod@gmail.com>
2020-06-16 16:39:49 +0100

    Setting up LibraryTest

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,2 +1,14 @@
+import org.junit.Before;
+
 public class LibraryTest {
+    private Library library;
+    private Book book1;
+    private Book book2;
+
+    @Before
+    public void before() {
+        library = new Library(2);
+        book1 = new Book("The Missing of Clairdelune", "Christelle Dabos", "Fantasy");
+        book2 = new Book("A Memory Called Empire", "Arkady Martine", "Science Fiction");
+    }
 }
