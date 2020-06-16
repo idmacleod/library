56bb317bfc73405aa017620336b56bc5ef1cc945
idmacleod <idmacleod@gmail.com>
2020-06-16 16:31:51 +0100

    Adding getter tests to BookTest

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookTest {
     private Book book;
@@ -6,5 +9,20 @@ public class BookTest {
     @Before
     public void before() {
         book = new Book("The Missing of Clairdelune", "Christelle Dabos", "Fantasy");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("The Missing of Clairdelune", book.getTitle());
+    }
+
+    @Test
+    public void hasAuthor() {
+        assertEquals("Christelle Dabos", book.getAuthor());
+    }
+
+    @Test
+    public void hasGenre() {
+        assertEquals("Fantasy", book.getGenre());
     }
 }
